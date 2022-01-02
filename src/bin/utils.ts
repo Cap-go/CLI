fa7ad5eb7038f50104bfd56b6503cfc2473d679c
martindonadieu <martindonadieu@gmail.com>
2022-01-03 00:23:54 +0100

    fix: host to service

--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -1,2 +1,2 @@
-// export const host = 'https://capacitorgo.com';
-export const host = 'http://localhost:3334';
+export const host = 'https://capacitorgo.com';
+// export const host = 'http://localhost:3334';
