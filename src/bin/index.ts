287e2d7acb9c02d07cc7ddb2f947d0024ea24f84
martindonadieu <martindonadieu@gmail.com>
2022-01-02 03:13:16 +0100

    fix: use appId as identifier

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -5,37 +5,35 @@ import axios from 'axios'
 
 program
   .option('-a, --apikey <apikey>', 'apikey to link to your account')
-  .option('-i, --icon <icon>', 'icon to link to your app')
   .option('-p, --production <production>', 'set version for production')
   .option('-p, --path <path>', 'path of the file to upload')
   .option('-v, --version <version>', 'version number of the file to upload')
-  .option('-n, --name <name>', 'name of the app to upload');
+  .option('-i, --appid <appid>', 'app id of the app to upload');
 
 program.parse(process.argv);
 const options = program.opts();
 
 start();
 async function start() {
-  let { name, apikey, version, path, icon, production } = options;
+  let { appid, apikey, version, path, production } = options;
   let config;
   try {
     config = await loadConfig();
   } catch {
     console.log('No capacitor config file found');
   }
-  name = name ? name : config?.app?.appName
+  appid = appid ? appid : config?.app?.appId
   version = version ? version : config?.app?.package?.version
   path = path ? path : config?.app?.webDir
-  icon = icon ? icon : ''
   if (!apikey) {
     console.log('You need to provide an API key to upload your app');
     return;
   }
-  if(!name || !version || !path) {
+  if(!appid || !version || !path) {
     console.log('You need to provide a name a version and a path or be in a capacitor project');
     return;
   }
-  console.log(`Upload ${name}@${version} from path ${path}`);
+  console.log(`Upload ${appid}@${version} from path ${path}`);
   try {
     const zip = new AdmZip();
     zip.addLocalFolder(path);
@@ -44,8 +42,7 @@ async function start() {
     // const host = "http://localhost:3334"
     const res = await axios.post(`${host}/api/upload`, {
       version,
-      name,
-      icon,
+      appid,
       mode: production ? 'prod' : 'dev',
       app: zip.toBuffer().toString('base64')
     }, {
