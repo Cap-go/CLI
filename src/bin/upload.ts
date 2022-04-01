ab78f0c3da6222c6e66d97e26128c21d71c237f9
martindonadieu <martindonadieu@gmail.com>
2022-04-01 19:52:14 +0100

    fix: put all env var in same file

--- src/bin/upload.ts
+++ src/bin/upload.ts
@@ -4,16 +4,9 @@ import axios from 'axios';
 import prettyjson from 'prettyjson';
 import { program } from 'commander';
 import cliProgress from 'cli-progress';
-import { host } from './utils';
+import { host, hostUpload, supaAnon } from './utils';
 
 const formatType = 'binary';
-/* eslint-disable */
-export const hostUpload = 'https://xvwzpoazmxkqosrdewyv.functions.supabase.co/upload';
-export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
-// For local test purposes
-// export const hostUpload = 'http://localhost:54321/functions/v1/upload';
-// export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'
-/* eslint-enable */
 
 export const uploadVersion = async (appid, options) => {
   let { version, path, channel } = options
--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -1,4 +1,9 @@
 /* eslint-disable */
 export const host = 'https://capgo.app';
+export const hostUpload = 'https://xvwzpoazmxkqosrdewyv.functions.supabase.co/upload';
+export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
+// For local test purposes
 // export const host = 'http://localhost:3334';
+// export const hostUpload = 'http://localhost:54321/functions/v1/upload';
+// export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'
 /* eslint-enable */
