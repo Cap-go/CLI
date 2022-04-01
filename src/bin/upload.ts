14a19def0a525c07978daed002c3018a4981df8a
martindonadieu <martindonadieu@gmail.com>
2022-04-01 18:53:15 +0100

    feat: use supbase function instead of netlify to handle timeout

--- src/bin/upload.ts
+++ src/bin/upload.ts
@@ -6,75 +6,68 @@ import { program } from 'commander';
 import cliProgress from 'cli-progress';
 import { host } from './utils';
 
-const oneMb = 1048576; // size of one mb
-const demiMb = oneMb / 2; // size of 1/2 mb
-const formatType = 'base64';
+const formatType = 'binary';
+/* eslint-disable */
+export const hostUpload = 'https://xvwzpoazmxkqosrdewyv.functions.supabase.co/upload';
+export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
+// For local test purposes
+// export const hostUpload = 'http://localhost:54321/functions/v1/upload';
+// export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs'
+/* eslint-enable */
 
 export const uploadVersion = async (appid, options) => {
-  let { version, path, channel } = options;
-  const { apikey } = options;
-  channel = channel || 'dev';
-  let config;
+  let { version, path, channel } = options
+  const { apikey } = options
+  channel = channel || 'dev'
+  let config
   try {
-    config = await loadConfig();
+    config = await loadConfig()
   } catch (err) {
-    program.error("No capacitor config file found, run `cap init` first");
+    program.error("No capacitor config file found, run `cap init` first")
   }
   appid = appid || config?.app?.appId
   version = version || config?.app?.package?.version
   path = path || config?.app?.webDir
   if (!apikey) {
-    program.error("Missing API key, you need to provide a API key to add your app");
+    program.error("Missing API key, you need to provide a API key to add your app")
   }
   if(!appid || !version || !path) {
-    program.error("Missing argument, you need to provide a appid and a version and a path, or be in a capacitor project");
+    program.error("Missing argument, you need to provide a appid and a version and a path, or be in a capacitor project")
   }
-  console.log(`Upload ${appid}@${version} from path ${path}`);
-  const b1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_grey);
+  console.log(`Upload ${appid}@${version} from path ${path}`)
+  const b1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_grey)
   try {
-    const zip = new AdmZip();
-    zip.addLocalFolder(path);
-    console.log('Uploading...');
-    const appData = zip.toBuffer().toString(formatType);
-    // split appData in chunks and send them sequentially with axios
-    const chunkSize = demiMb;
-    const chunks = [];
-    for (let i = 0; i < appData.length; i += chunkSize) {
-      chunks.push(appData.slice(i, i + chunkSize));
-    }
-    b1.start(chunks.length, 0, {
+    const zip = new AdmZip()
+    zip.addLocalFolder(path)
+    console.log('Uploading...')
+    const appData = zip.toBuffer().toString(formatType)
+    b1.start(1, 0, {
       speed: "N/A"
-    });
-    let fileName
-    for (let i = 0; i < chunks.length; i +=1) {
-      const res = await axios({
-        method: 'POST',
-        url:`${host}/api/upload`,
-        data: {
-          version,
-          appid,
-          fileName,
-          channel,
-          format: formatType,
-          app: chunks[i],
-          isMultipart: chunks.length > 1,
-          chunk: i + 1,
-          totalChunks: chunks.length,
-        },
-        validateStatus: () => true,
-        headers: {
-          'authorization': apikey
-        }})
-      if (res.status !== 200) {
-        program.error(`Server Error \n${prettyjson.render(res.data)}`);
-      }
-      b1.update(i+1)
-      fileName = res.data.fileName
+    })
+    const res = await axios({
+      method: 'POST',
+      url: hostUpload,
+      data: {
+        version,
+        appid,
+        channel,
+        format: formatType,
+        app: appData,
+      },
+      validateStatus: () => true,
+      headers: {
+        'Content-Type': 'application/json',
+        'apikey': apikey,
+        authorization : `Bearer ${supaAnon}`
+      }})
+    if (res.status !== 200) {
+      program.error(`Server Error \n${prettyjson.render(res.data)}`)
     }
-    b1.stop();
+    b1.update(1)
+    b1.stop()
   } catch (err) {
-    b1.stop();
-    program.error(`Network Error \n${prettyjson.render(err.response.data)}`);
+    b1.stop()
+    program.error(`Network Error \n${prettyjson.render(err.response.data)}`)
   }
   console.log("App uploaded to server")
   console.log(`Try it in mobile app: ${host}`)
--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -1,2 +1,4 @@
+/* eslint-disable */
 export const host = 'https://capgo.app';
 // export const host = 'http://localhost:3334';
+/* eslint-enable */
