03041a3df460b075437a998baaf7d760d6a0cecc
martindonadieu <martindonadieu@gmail.com>
2022-04-06 14:23:14 +0100

    feat: allow external url for upload

--- src/bin/upload.ts
+++ src/bin/upload.ts
@@ -12,7 +12,7 @@ const formatType = 'base64';
 
 export const uploadVersion = async (appid, options) => {
   let { version, path, channel } = options;
-  const { apikey } = options;
+  const { apikey, external } = options;
   channel = channel || 'dev';
   let config;
   try {
@@ -30,36 +30,16 @@ export const uploadVersion = async (appid, options) => {
     program.error("Missing argument, you need to provide a appid and a version and a path, or be in a capacitor project");
   }
   console.log(`Upload ${appid}@${version} started from path "${path}" to Capgo cloud`);
-  const b1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_grey);
-  try {
-    const zip = new AdmZip();
-    zip.addLocalFolder(path);
-    console.log('Uploading:');
-    const appData = zip.toBuffer().toString(formatType);
-    // split appData in chunks and send them sequentially with axios
-    const chunkSize = oneMb;
-    const chunks = [];
-    for (let i = 0; i < appData.length; i += chunkSize) {
-      chunks.push(appData.slice(i, i + chunkSize));
-    }
-    b1.start(chunks.length, 0, {
-      speed: "N/A"
-    });
-    let fileName
-    for (let i = 0; i < chunks.length; i +=1) {
+  if (external) {
+    try {
       const res = await axios({
         method: 'POST',
         url: hostUpload,
         data: {
           version,
           appid,
-          fileName,
           channel,
-          format: formatType,
-          app: chunks[i],
-          isMultipart: chunks.length > 1,
-          chunk: i + 1,
-          totalChunks: chunks.length,
+          external,
         },
         validateStatus: () => true,
         headers: {
@@ -68,21 +48,71 @@ export const uploadVersion = async (appid, options) => {
           authorization : `Bearer ${supaAnon}`
         }})
       if (res.status !== 200) {
-        b1.stop();
         program.error(`Server Error \n${prettyjson.render(res?.data || "")}`);
       }
-      b1.update(i+1)
-      fileName = res.data.fileName
+    } catch (err) {
+      if (err.response) {
+        program.error(`Network Error \n${prettyjson.render(err.response?.data || "")}`)
+      } else {
+        program.error(`Network Error \n${prettyjson.render(err || "")}`)
+      }
     }
-    b1.stop();
-  } catch (err) {
-    b1.stop();
-    if (err.response) {
-      program.error(`Network Error \n${prettyjson.render(err.response?.data || "")}`)
-    } else {
-      program.error(`Network Error \n${prettyjson.render(err || "")}`)
+  } else {
+    const b1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_grey);
+    try {
+      const zip = new AdmZip();
+      zip.addLocalFolder(path);
+      console.log('Uploading:');
+      const appData = zip.toBuffer().toString(formatType);
+      // split appData in chunks and send them sequentially with axios
+      const chunkSize = oneMb;
+      const chunks = [];
+      for (let i = 0; i < appData.length; i += chunkSize) {
+        chunks.push(appData.slice(i, i + chunkSize));
+      }
+      b1.start(chunks.length, 0, {
+        speed: "N/A"
+      });
+      let fileName
+      for (let i = 0; i < chunks.length; i +=1) {
+        const res = await axios({
+          method: 'POST',
+          url: hostUpload,
+          data: {
+            version,
+            appid,
+            fileName,
+            channel,
+            format: formatType,
+            app: chunks[i],
+            isMultipart: chunks.length > 1,
+            chunk: i + 1,
+            totalChunks: chunks.length,
+          },
+          validateStatus: () => true,
+          headers: {
+            'Content-Type': 'application/json',
+            'apikey': apikey,
+            authorization : `Bearer ${supaAnon}`
+          }})
+        if (res.status !== 200) {
+          b1.stop();
+          program.error(`Server Error \n${prettyjson.render(res?.data || "")}`);
+        }
+        b1.update(i+1)
+        fileName = res.data.fileName
+      }
+      b1.stop();
+    } catch (err) {
+      b1.stop();
+      if (err.response) {
+        program.error(`Network Error \n${prettyjson.render(err.response?.data || "")}`)
+      } else {
+        program.error(`Network Error \n${prettyjson.render(err || "")}`)
+      }
     }
   }
+
   console.log("App uploaded to server")
   console.log(`Try it in mobile app: ${host}/app_mobile`)
   console.log(`Or set the channel ${channel} as public here: ${host}/app/package/${appid}`)
