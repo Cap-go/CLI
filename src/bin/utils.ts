1f976ac18fcb6d197a7c6e5b3c24f0d5fdf035d7
martindonadieu <martindonadieu@gmail.com>
2022-06-01 03:09:15 +0100

    fix: use the prod env

--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -8,15 +8,15 @@ import { definitions } from './types_supabase';
 
 export const host = 'https://capgo.app';
 export const hostWeb = 'https://web.capgo.app';
-// export const hostSupa = 'https://xvwzpoazmxkqosrdewyv.functions.supabase.co';
-export const hostSupa = 'https://aucsybvnhavogdmzwtcw.supabase.co';
+export const hostSupa = 'https://xvwzpoazmxkqosrdewyv.functions.supabase.co';
+// export const hostSupa = 'https://aucsybvnhavogdmzwtcw.supabase.co';
 
 // For local test purposes
 // export const host = 'http://localhost:3334';
 
 /* eslint-disable */
-// export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
-export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1Y3N5YnZuaGF2b2dkbXp3dGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODcxNjgsImV4cCI6MTk2OTE2MzE2OH0.8FKKJqiGgoVA3p9GH5wvnbWkWywIxVLqQyZFhupZ7C4'
+export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
+// export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1Y3N5YnZuaGF2b2dkbXp3dGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODcxNjgsImV4cCI6MTk2OTE2MzE2OH0.8FKKJqiGgoVA3p9GH5wvnbWkWywIxVLqQyZFhupZ7C4'
 /* eslint-enable */
 
 export const createSupabaseClient = (apikey: string) => createClient(hostSupa, supaAnon, {
