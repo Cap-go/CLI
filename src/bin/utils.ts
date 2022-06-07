c02eb3f84bf29ea0fe1cb7dc75d2b4c16bab0195
martindonadieu <martindonadieu@gmail.com>
2022-06-07 14:21:32 +0100

    fix: remove useless comment

--- src/bin/utils.ts
+++ src/bin/utils.ts
@@ -11,9 +11,6 @@ export const hostWeb = 'https://web.capgo.app';
 export const hostSupa = 'https://xvwzpoazmxkqosrdewyv.supabase.co';
 // export const hostSupa = 'https://aucsybvnhavogdmzwtcw.supabase.co';
 
-// For local test purposes
-// export const host = 'http://localhost:3334';
-
 /* eslint-disable */
 export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzgwNTAwOSwiZXhwIjoxOTUzMzgxMDA5fQ.8tgID1d4jodPwuo_fz4KHN4o1XKB9fnqyt0_GaJSj-w'
 // export const supaAnon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1Y3N5YnZuaGF2b2dkbXp3dGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTM1ODcxNjgsImV4cCI6MTk2OTE2MzE2OH0.8FKKJqiGgoVA3p9GH5wvnbWkWywIxVLqQyZFhupZ7C4'
