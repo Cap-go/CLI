e5883eb8191f6c7e49ee00191a80f2debc00db4d
Arend-Jan Tetteroo <arend-jantetteroo@sofios.nl>
2022-11-14 14:14:17 +0100

    more refactoring

--- src/bin/cleanup.ts
+++ src/bin/cleanup.ts
@@ -39,6 +39,39 @@ function getHumanDate(row: AppVersion) {
   return date.toLocaleString();
 }
 
+async function checkAppExistsAndHasPermission(supabase: SupabaseClient, appid: string, apikey: string) {
+  const { data: app, error: dbError0 } = await supabase
+    .rpc<string>('exist_app', { appid, apikey });
+  if (!app || dbError0) {
+    program.error('No permission for this app');
+  }
+}
+
+async function getActiveAppVersions(supabase: SupabaseClient, appid: string, userId: string) {
+  const { data, error: vError } = await supabase
+    .from<definitions['app_versions']>('app_versions')
+    .select()
+    .eq('app_id', appid)
+    .eq('user_id', userId)
+    .eq('deleted', false);
+
+  if (vError) {
+    program.error(`App ${appid} not found in database ${vError} `);
+  }
+  return data;
+}
+
+function getRemovableVersionsInSemverRange(data: AppVersion[], bundle: string, nextMajor: string) {
+  const toRemove: AppVersion[] = [];
+
+  data?.forEach(row => {
+    if (semver.gte(row.name, bundle) && semver.lt(row.name, `${nextMajor}`)) {
+      toRemove.push(row);
+    }
+  });
+  return toRemove;
+}
+
 export const cleanupApp = async (appid: string, options: Options) => {
   const apikey = options.apikey || findSavedKey()
   const { bundle } = options;
@@ -63,33 +96,20 @@ export const cleanupApp = async (appid: string, options: Options) => {
 
   const userId = await verifyUser(supabase, apikey);
 
-  const { data: app, error: dbError0 } = await supabase
-    .rpc<string>('exist_app', { appid, apikey })
-  if (!app || dbError0) {
-    program.error('No permission for this app')
-  }
+  // Check we have app access to this appId
+  await checkAppExistsAndHasPermission(supabase, appid, apikey);
 
-  const { data, error: vError } = await supabase
-    .from<definitions['app_versions']>('app_versions')
-    .select()
-    .eq('app_id', appid)
-    .eq('user_id', userId)
-    .eq('deleted', false)
-
-  if (vError) {
-    program.error(`App ${appid} not found in database ${vError} `)
-  }
+  // Get all active app versions we might possibly be able to cleanup
+  const data = await getActiveAppVersions(supabase, appid, userId);
 
   console.log(`Total active versions in Capgo: ${data?.length}`);
 
-  const toRemove: AppVersion[] = [];
+  if (data?.length === 0) {
+    return;
+  }
 
-  // Check what to remove in between the major ranges
-  data?.forEach(row => {
-    if (semver.gte(row.name, bundle) && semver.lt(row.name, `${nextMajor}`)) {
-        toRemove.push(row);
-    }
-  });
+  // Get all app versions that are in the given range
+  const toRemove = getRemovableVersionsInSemverRange(data, bundle, nextMajor);
 
   console.log(`Active versions in Capgo between ${bundle} and ${nextMajor}: ${toRemove?.length}`);
 
@@ -119,16 +139,19 @@ export const cleanupApp = async (appid: string, options: Options) => {
     return;
   }
 
+  // Show the user what will be removed
   toRemove?.forEach(row => {
     console.log(`${row.name} created on ${(getHumanDate(row))} will be removed`);
   });
 
+  // Check user wants to clean that all up
   const result = prompt("Do you want to continue removing the versions specified? Type yes to confirm");
   if (result !== "yes") {
     console.log("Not confirmed, aborting removal...");
     return;
   }
 
+  // Yes, lets clean it up
   console.log("You have confiremd removal, removing versions now");
   removeVersions(toRemove, supabase, appid, userId);
 }
