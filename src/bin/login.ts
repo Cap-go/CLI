a470e1668b5cde8fe8b503fb805598628e78184c
martindonadieu <martindonadieu@gmail.com>
2022-12-28 03:02:59 +0800

    fix: login missing try catch

--- src/bin/login.ts
+++ src/bin/login.ts
@@ -8,29 +8,34 @@ interface Options {
 }
 export const login = async (apikey: string, options: Options) => {
   // write in file .capgo the apikey in home directory
-  const { local } = options;
-  const snag = useLogSnag()
+  try {
+    const { local } = options;
+    const snag = useLogSnag()
 
-  if (local) {
-    if (!existsSync('.git')) {
-      program.error('To use local you should be in a git repository');
+    if (local) {
+      if (!existsSync('.git')) {
+        program.error('To use local you should be in a git repository');
+      }
+      writeFileSync('.capgo', `${apikey}\n`);
+      appendFileSync('.gitignore', '.capgo\n');
+    } else {
+      const userHomeDir = homedir();
+      writeFileSync(`${userHomeDir}/.capgo`, `${apikey}\n`);
     }
-    writeFileSync('.capgo', `${apikey}\n`);
-    appendFileSync('.gitignore', '.capgo\n');
-  } else {
-    const userHomeDir = homedir();
-    writeFileSync(`${userHomeDir}/.capgo`, `${apikey}\n`);
+    const supabase = createSupabaseClient(apikey)
+    const userId = await verifyUser(supabase, apikey, ['write', 'all', 'upload']);
+    snag.publish({
+      channel: 'user-login',
+      event: 'User CLI login',
+      icon: '✅',
+      tags: {
+        'user-id': userId,
+      },
+      notify: false,
+    }).catch()
+    console.log(`login saved into .capgo file in ${local ? 'local' : 'home'} directory`);
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
   }
-  const supabase = createSupabaseClient(apikey)
-  const userId = await verifyUser(supabase, apikey, ['write', 'all', 'upload']);
-  snag.publish({
-    channel: 'user-login',
-    event: 'User CLI login',
-    icon: '✅',
-    tags: {
-      'user-id': userId,
-    },
-    notify: false,
-  }).catch()
-  console.log(`login saved into .capgo file in ${local ? 'local' : 'home'} directory`);
 }
