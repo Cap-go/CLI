d05282878df55236a20b19bd6d8f99cbfe5bae20
Martin Donadieu <martindonadieu@gmail.com>
2024-04-23 15:52:20 +0100

    fix: make node18 as min version

--- build.mjs
+++ build.mjs
@@ -1,23 +1,23 @@
-import * as esbuild from 'esbuild'
+import * as esbuild from "esbuild";
 
 // Replace 'your-external-dependencies-here' with actual externals from your project
 const external = [];
 
 esbuild.build({
-  entryPoints: ['src/index.ts'],
+  entryPoints: ["src/index.ts"],
   bundle: true,
-  platform: 'node',
-  target: 'node20', 
+  platform: "node",
+  target: "node18",
   external,
-  outdir: 'dist',
-  sourcemap: process.env.NODE_ENV === 'development',
+  outdir: "dist",
+  sourcemap: process.env.NODE_ENV === "development",
   banner: {
-    js: '#!/usr/bin/env node',
+    js: "#!/usr/bin/env node",
   },
   define: {
-    'process.env.SUPA_DB': '"production"',
+    "process.env.SUPA_DB": '"production"',
   },
   loader: {
-    '.ts': 'ts',
+    ".ts": "ts",
   },
 }).catch(() => process.exit(1));
