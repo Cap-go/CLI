8621ec0899e14e3386d5c9899937ae56dca19495
Martin Donadieu <martindonadieu@gmail.com>
2024-07-18 17:48:58 +0300

    fix: lint mjs

--- build.mjs
+++ build.mjs
@@ -1,21 +1,22 @@
-import * as esbuild from "esbuild";
+import { exit } from 'node:process'
+import * as esbuild from 'esbuild'
 
 esbuild.build({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ['src/index.ts'],
   bundle: true,
-  platform: "node",
-  target: "node18",
-  outfile: "dist/index.js", // Change this to output a single file
-  sourcemap: process.env.NODE_ENV === "development",
+  platform: 'node',
+  target: 'node18',
+  outfile: 'dist/index.js', // Change this to output a single file
+  sourcemap: process.env.NODE_ENV === 'development',
   minify: true, // Minify the output
   treeShaking: true, // Enable tree shaking to remove unused code
   banner: {
-    js: "#!/usr/bin/env node",
+    js: '#!/usr/bin/env node',
   },
   define: {
-    "process.env.SUPA_DB": '"production"',
+    'process.env.SUPA_DB': '"production"',
   },
   loader: {
-    ".ts": "ts",
+    '.ts': 'ts',
   },
-}).catch(() => process.exit(1));
+}).catch(() => exit(1))
