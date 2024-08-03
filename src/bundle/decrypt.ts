6e8dcc5a49895b039abb4ae5229a57dda055a422
Martin Donadieu <martindonadieu@gmail.com>
2024-08-03 14:41:20 +0100

    fix: error in decrypt code

--- src/bundle/decrypt.ts
+++ src/bundle/decrypt.ts
@@ -21,20 +21,19 @@ export async function decryptZip(zipPath: string, ivsessionKey: string, options:
     program.error('')
   }
 
-  const config = await getConfig()
-  const { extConfig } = config.app
+  const extConfig = await getConfig()
 
-  if (!options.key && !existsSync(baseKey) && !extConfig.plugins?.CapacitorUpdater?.privateKey) {
-    log.error(`Private Key not found at the path ${baseKey} or in ${config.apextConfigFilePath}`)
+  if (!options.key && !existsSync(baseKey) && !extConfig.config.plugins?.CapacitorUpdater?.privateKey) {
+    log.error(`Private Key not found at the path ${baseKey} or in ${extConfig.path}`)
     program.error('')
   }
   const keyPath = options.key || baseKey
   // check if publicKey exist
 
-  let privateKey = extConfig?.plugins?.CapacitorUpdater?.privateKey
+  let privateKey = extConfig.config.plugins?.CapacitorUpdater?.privateKey
 
   if (!existsSync(keyPath) && !privateKey) {
-    log.error(`Cannot find public key ${keyPath} or as keyData option or in ${config.apextConfigFilePath}`)
+    log.error(`Cannot find public key ${keyPath} or as keyData option or in ${extConfig.path}`)
     program.error('')
   }
   else if (existsSync(keyPath)) {
