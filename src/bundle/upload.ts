10b290dd9497ca131dbb1a0df18e8db6dd3b3865
Martin Donadieu <martindonadieu@gmail.com>
2024-08-06 19:40:05 +0100

    fix: comment partial for lint

--- src/bundle/upload.ts
+++ src/bundle/upload.ts
@@ -1,9 +1,7 @@
 import { randomUUID } from 'node:crypto'
-import { createReadStream, existsSync, readFileSync } from 'node:fs'
+import { existsSync, readFileSync } from 'node:fs'
 import { exit } from 'node:process'
 import type { Buffer } from 'node:buffer'
-import { createGzip } from 'node:zlib'
-import { buffer as readBuffer } from 'node:stream/consumers'
 import { program } from 'commander'
 import { checksum as getChecksum } from '@tomasklaen/checksum'
 import ciDetect from 'ci-info'
@@ -13,8 +11,8 @@ import ky, { HTTPError } from 'ky'
 import { confirm as confirmC, intro, log, outro, spinner as spinnerC } from '@clack/prompts'
 import type { Database } from '../types/supabase.types'
 import { encryptSource } from '../api/crypto'
-import type { OptionsBase, manifestType, uploadUrlsType } from '../utils'
-import { OrganizationPerm, baseKeyPub, checkChecksum, checkCompatibility, checkPlanValid, convertAppName, createSupabaseClient, deletedFailedVersion, findSavedKey, formatError, generateManifest, getConfig, getLocalConfig, getLocalDepenencies, getOrganizationId, getPMAndCommand, hasOrganizationPerm, manifestUploadUrls, readPackageJson, regexSemver, updateOrCreateChannel, updateOrCreateVersion, uploadMultipart, uploadUrl, useLogSnag, verifyUser, zipFile } from '../utils'
+import type { OptionsBase } from '../utils'
+import { OrganizationPerm, baseKeyPub, checkChecksum, checkCompatibility, checkPlanValid, convertAppName, createSupabaseClient, deletedFailedVersion, findSavedKey, formatError, getConfig, getLocalConfig, getLocalDepenencies, getOrganizationId, getPMAndCommand, hasOrganizationPerm, readPackageJson, regexSemver, updateOrCreateChannel, updateOrCreateVersion, uploadMultipart, uploadUrl, useLogSnag, verifyUser, zipFile } from '../utils'
 import { checkAppExistsAndHasPermissionOrgErr } from '../api/app'
 import { checkLatest } from '../api/update'
 import type { CapacitorConfig } from '../config'
@@ -232,72 +230,72 @@ async function checkVersionExists(supabase: SupabaseType, appid: string, bundle:
   }
 }
 
-async function prepareBundlePartialFiles(path: string, snag: LogSnag, orgId: string, appid: string) {
-  const spinner = spinnerC()
-  spinner.start('Generating the update manifest')
-  const manifest = await generateManifest(path)
-  spinner.stop('Manifest generated successfully')
+// async function prepareBundlePartialFiles(path: string, snag: LogSnag, orgId: string, appid: string) {
+//   const spinner = spinnerC()
+//   spinner.start('Generating the update manifest')
+//   const manifest = await generateManifest(path)
+//   spinner.stop('Manifest generated successfully')
 
-  await snag.track({
-    channel: 'partial-update',
-    event: 'Generate manifest',
-    icon: '📂',
-    user_id: orgId,
-    tags: {
-      'app-id': appid,
-    },
-    notify: false,
-  }).catch()
+//   await snag.track({
+//     channel: 'partial-update',
+//     event: 'Generate manifest',
+//     icon: '📂',
+//     user_id: orgId,
+//     tags: {
+//       'app-id': appid,
+//     },
+//     notify: false,
+//   }).catch()
 
-  return manifest
-}
+//   return manifest
+// }
 
-async function uploadPartial(apikey: string, manifest: manifestType, path: string, options: Options, appId: string, name: string) {
-  const spinner = spinnerC()
-  spinner.start('Preparing partial update')
-  const uploadResponse: uploadUrlsType[] = await manifestUploadUrls(apikey, appId, name, manifest)
+// async function uploadPartial(apikey: string, manifest: manifestType, path: string, options: Options, appId: string, name: string) {
+//   const spinner = spinnerC()
+//   spinner.start('Preparing partial update')
+//   const uploadResponse: uploadUrlsType[] = await manifestUploadUrls(apikey, appId, name, manifest)
 
-  if (uploadResponse.length === 0 || uploadResponse.length !== manifest.length) {
-    log.error(`Cannot upload manifest, please try again later`)
-    spinner.stop('Partial update failed')
-    return []
-  }
-  spinner.message('Uploading partial update')
-  for (const [index, manifestEntry] of uploadResponse.entries()) {
-    const finalFilePath = `${path}/${manifestEntry.path}`
-    spinner.message(`Uploading partial update ${index + 1}/${uploadResponse.length}`)
-    const fileStream = createReadStream(finalFilePath).pipe(createGzip({ level: 9 }))
-    const fileBuffer = await readBuffer(fileStream)
+//   if (uploadResponse.length === 0 || uploadResponse.length !== manifest.length) {
+//     log.error(`Cannot upload manifest, please try again later`)
+//     spinner.stop('Partial update failed')
+//     return []
+//   }
+//   spinner.message('Uploading partial update')
+//   for (const [index, manifestEntry] of uploadResponse.entries()) {
+//     const finalFilePath = `${path}/${manifestEntry.path}`
+//     spinner.message(`Uploading partial update ${index + 1}/${uploadResponse.length}`)
+//     const fileStream = createReadStream(finalFilePath).pipe(createGzip({ level: 9 }))
+//     const fileBuffer = await readBuffer(fileStream)
 
-    try {
-      await ky.put(manifestEntry.uploadLink, {
-        timeout: options.timeout || UPLOAD_TIMEOUT,
-        retry: 5,
-        body: fileBuffer,
-      })
-    }
-    catch (errorUpload) {
-      if (errorUpload instanceof HTTPError) {
-        errorUpload.response.text()
-          .then(body => log.error(`Response: ${formatError(body)}`))
-          .catch(() => log.error('Cannot get response body'))
-      }
-      else {
-        console.error(errorUpload)
-      }
-      return null
-    }
-  }
+//     try {
+//       await ky.put(manifestEntry.uploadLink, {
+//         timeout: options.timeout || UPLOAD_TIMEOUT,
+//         retry: 5,
+//         body: fileBuffer,
+//       })
+//     }
+//     catch (errorUpload) {
+//       if (errorUpload instanceof HTTPError) {
+//         errorUpload.response.text()
+//           .then(body => log.error(`Response: ${formatError(body)}`))
+//           .catch(() => log.error('Cannot get response body'))
+//       }
+//       else {
+//         console.error(errorUpload)
+//       }
+//       return null
+//     }
+//   }
 
-  spinner.stop('Partial update uploaded successfully')
-  return uploadResponse.map((entry) => {
-    return {
-      file_name: entry.path,
-      s3_path: entry.finalPath,
-      file_hash: entry.hash,
-    }
-  })
-}
+//   spinner.stop('Partial update uploaded successfully')
+//   return uploadResponse.map((entry) => {
+//     return {
+//       file_name: entry.path,
+//       s3_path: entry.finalPath,
+//       file_hash: entry.hash,
+//     }
+//   })
+// }
 
 async function prepareBundleFile(path: string, options: Options, localConfig: localConfigType, snag: LogSnag, orgId: string, appid: string) {
   let sessionKey
