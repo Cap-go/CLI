08fa86304708b1b62317e398d7dba972e1378ad6
WcaleNieWolny <isupermichael007@gmail.com>
2025-09-16 15:41:23 +0200

    feat: limit retention to 2 years

--- src/app/set.ts
+++ src/app/set.ts
@@ -38,6 +38,9 @@ export async function setApp(appId: string, options: Options) {
   else if (retention && retention < 0) {
     log.error(`retention value cannot be less than 0`)
     program.error(``)
+  } else if (retention && retention >= 63113904) {
+    log.error(`retention value cannot be greater than 63113904 seconds (2 years)`)
+    program.error(``)
   }
 
   let iconBuff
